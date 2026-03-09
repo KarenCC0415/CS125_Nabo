78a727c134aee2223790195987fed27767824ba3
Ryan Matthew Hua <rmhua8@gmail.com>
2026-03-08 23:14:07 -0700

    test

--- scripts/api.js
+++ scripts/api.js
@@ -5,21 +5,38 @@ import { computeSimilarityScore } from './ranking.js'
 const userQuery = "asian food";
 const userPreferences = ["chinese", "korean"];
 
-const options = {
-  method: 'GET',
-  url: 'https://api.hasdata.com/scrape/google-maps/search',
-  params: {q: userQuery, ll: '@34.0522,-118.2437,14z'},
-  headers: {
-    'x-api-key': HASDATA_MAPS_API_KEY,
-    'Content-Type': 'application/json'
-  }
-};
+async function fetchAllResults(userQuery) {
+  const allResults = [];
+  let nextPageToken = null;
+  const maxPages = 3;
 
+  for (let page = 0; page < maxPages; page++) {
+    const params = { q: userQuery, ll: '@34.0522,-118.2437,14z' };
+    if (nextPageToken) params.nextPageToken = nextPageToken;
+
+    const { data } = await axios.request({
+      method: 'GET',
+      url: 'https://api.hasdata.com/scrape/google-maps/search',
+      params,
+      headers: {
+        'x-api-key': HASDATA_MAPS_API_KEY,
+        'Content-Type': 'application/json'
+      }
+    });
+
+    if (data.localResults) allResults.push(...data.localResults);
+
+    nextPageToken = data.nextPageToken || null;
+    if (!nextPageToken) break;
+  }
+
+  return allResults;
+}
 
 try {
-  const { data } = await axios.request(options);
+  const allPlaces = await fetchAllResults(userQuery);
 
-  const rankedResults = data.localResults
+  const rankedResults = allPlaces
     .map(place => ({
       place,
       score: computeSimilarityScore(place, userQuery, userPreferences)
